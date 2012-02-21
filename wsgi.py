29997081cf3dd43caaeb699c8c57c1f0196a5b58
Ken Cochrane <kencochrane@gmail.com>
2012-02-21 09:30:13 -0500

    added example wsgi.py script

---
+++ wsgi.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# This is just an example, replace this with your own wsgi.py file
+
+def app(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return ['Hello, world!\n']
